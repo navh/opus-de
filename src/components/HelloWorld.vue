505bdbf6056b04d884c63faca264adf2fc789a2e
navh <navarre.hebb@gmail.com>
2020-06-14 22:36:11 -0300

    test cloudbuild hook

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,7 +27,7 @@
         >
       </li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>Important Links</h3>
     <ul>
       <li>
         <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
